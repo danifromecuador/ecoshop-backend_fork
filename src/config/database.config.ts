5028e673126161b6733de65102fa50c8f434b11c
Daniel Morillo <dev-07@mennobot.com>
2025-12-04 13:55:12 -0500

    change database url

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  uri: process.env.MONGO_DB_URI || 'mongodb://localhost:27017/ecoshop',
+  uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/ecoshop',
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
